f2a4cf03e2bd2fb50a0d5d8e90bc99cba72101ed
PolluxAvenger <polluxavenger@gmail.com>
2016-08-31 22:07:33 +0800

    完善 C 段扫描功能
    调用 nmap 来进行扫描，未进行参数优化时间略长，原来的扫描退化成简单 HTTP 开放验证扫描，端口开放存储在 port.json 文件中

--- C-segment_scan.py
+++ C-segment_scan.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import nmap
+import json
 import socket
 import requests
 from concurrent.futures import ThreadPoolExecutor
@@ -8,15 +10,60 @@ from concurrent.futures import ThreadPoolExecutor
 def worker(ip_address):
     global result_list
 
-    #print('正在搜索' + ip_address)
+    print('正在搜索' + ip_address)
 
-    for port in range(1, 1025):
-        link = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        link.settimeout(2)
-        status = link.connect_ex((str(ip_address), int(port)))
+    try:
+        nmap_Scan = nmap.PortScanner()
+        nmap_Scan.scan(ip_address, '22-443')
+    except Exception as e:
+        print(str(e))
 
-        if status == 0:
-            result_list.append(str(ip_address + ':' + str(port)))
+    for host in nmap_Scan.all_hosts():
+        port_info = []
+
+        for protocol in nmap_Scan[host].all_protocols():
+            try:
+                lport = nmap_Scan[host][protocol].keys()
+            except Exception as e:
+                print(str(e))
+                continue
+
+            for port in lport:
+                try:
+                    port_info.append({'port': port, 'status': nmap_Scan[host][protocol][port]['state']})
+                except Exception as e:
+                    print(str(e))
+        # 还缺少一个对 port_info 是否为空的判断，为空就不写入了
+        try:
+            result = {ip_address: port_info}
+        except Exception as e:
+            print(str(e))
+
+    print(ip_address + '完成搜索')
+    result_list.append(result)
+    print(ip_address + '写入结果完毕')
+
+
+def http_alive(ip_address):
+    global result_list
+
+    for port in [80, 443, 8080]:
+       link = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       link.settimeout(2)
+       status = link.connect_ex((str(ip_address), int(port)))
+
+       if status == 0:
+           result_list.append(str(ip_address + ':' + str(port)))
+
+		   
+def long_data(data):
+    with open('port.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def ip2domain():
+    # 准备从 http://ip.aa2.cn 获得域名信息
+    print('B')
 
 
 if __name__ == '__main__':
@@ -27,11 +74,11 @@ if __name__ == '__main__':
     ip_address = result['ip'].split('.')
     net_address = '.'.join(ip_address[:-1])
 
-    # net_address = '123.58.180' 网易验证 180.149.134.141 新浪验证
+    # net_address = '123.58.180' # 网易验证 180.149.134.141 新浪验证
 
     with ThreadPoolExecutor(max_workers=10) as executor:
-        for item in range(1, 256):
+        for item in range(8, 10):
             ip = '%s.%s' % (net_address, item)
             executor.submit(worker, ip)
 
-    print(result_list)
+    long_data(result_list)
