337e5190b8ce670aad136dff57d42c017e2e6f2e
PolluxAvenger <polluxavenger@gmail.com>
2017-02-10 14:52:13 +0800

    增加 DGA 跟踪源
    OSINT 的 DGA 家族跟踪数据

---
+++ threat_intelligence/OSINT_master.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+import os
+import csv
+import requests
+
+
+def extract_data(url):
+    file_name = os.path.basename(url)
+    r = requests.get(url, stream=True)
+    file_type = file_name.split('.')[0]
+    local_name = os.path.join(os.getcwd(), 'dga', (file_type + '.data'))
+    ip_list = []
+    os.makedirs(os.path.dirname(local_name), exist_ok=True)
+
+    with open((file_type + '_row.txt'), "wb") as t:
+        for chunk in r.iter_content(chunk_size=512):
+            if chunk:
+                t.write(chunk)
+
+    with open((file_type + '_row.txt'), "rb") as x:
+        count = 0
+        for item in x:
+            if count < 14:
+                count += 1
+                continue
+            split_result = str(item).split(',')
+            ip_list.append({'Domain': split_result[0],
+                            'Date': split_result[2],
+                            'Description': split_result[1]})
+
+    os.remove(file_type + '_row.txt')
+
+    with open('dga_all_track.csv', 'w', newline='', encoding='utf-8') as t:
+        headers = ['Domain', 'Date', 'Description']
+        writer = csv.DictWriter(t, headers)
+        writer.writeheader()
+
+        for item in ip_list:
+            writer.writerow(item)
+
+
+if __name__ == '__main__':
+    extract_data('http://osint.bambenekconsulting.com/feeds/dga-feed.txt')
