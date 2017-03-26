8b8dcc089f73dbeda64ed451a94c84d25395a2aa
PolluxAvenger <polluxavenger@gmail.com>
2017-03-26 17:15:16 +0800

    CNNIC IPv6 查询
    IPv6 信息查询接口

---
+++ CNNIC/cnnic_ipv6.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+import bs4
+import pickle
+import urllib.request
+
+
+def ipv6_data(person_info, result_list):
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'
+    }
+
+    url = 'http://ipwhois.cnnic.net.cn/bns/query/Query/ipwhoisQuery.do?txtquery=' + person_info + '&queryOption=ipv6'
+    request = urllib.request.Request(url, headers=header)
+    response = urllib.request.urlopen(request)
+    content = response.read()
+    result = bs4.BeautifulSoup(content, "html.parser")
+    link = result.find_all('tr')
+    count = 0
+
+    for item in link:
+        count += 1
+        if item.contents[1].text.split(':')[0] == '':
+            continue
+        elif count <= 12:
+            result_list.append({item.contents[1].text.split(':')[0] : item.contents[3].text.replace(u'\xa0', '')})
+        elif count >= 14 and count <= 23:
+            result_list.append({item.contents[1].text.split(':')[0] : item.contents[3].text.replace(u'\xa0', '')})
+        elif count > 23:
+            break
+        else:
+            result_list.append({item.contents[1].text.split(':')[0]: item.contents[3].text.replace(u'\xa0', '')})
+
+    with open('cnnic_ipv6.data', 'wb') as f:
+        pickle.dump(result_list, f)
+
+    return result_list
+
+
+if __name__ == "__main__":
+    data_list = []
+    data_list = ipv6_data('2001:F88::', data_list)
