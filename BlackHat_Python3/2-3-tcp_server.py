f914a7c30ff4ced03a4ca0788b5aa4a52f29e98e
PolluxAvenger <polluxavenger@gmail.com>
2017-04-16 22:19:00 +0800

    BlackHat Python3
    TCP 多线程服务器

---
+++ BlackHat_Python3/2-3-tcp_server.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
+server.listen(10)
+print("[*] Litening on %s:%d" % (bind_ip, bind_port))
+
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    print("[*] Received: %s" % request)
+    client_socket.send(b"ACK!\r\n")
+    client_socket.close()
+
+
+while True:
+    client, addr = server.accept()
+    print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
+
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
